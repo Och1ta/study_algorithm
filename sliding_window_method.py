3985a1f62528ca3cc6a73ebff8c84e61dd9ac83c
Anton Kabanov <imcreativchik@gmail.com>
2023-10-10 15:00:49 +0500

    sliding window method

---
+++ sliding_window_method.py
@@ -0,0 +1,20 @@
+# Для массива, состоящего из n целых чисел,
+# найдите непрерывный подмассив (срез) заданной длины k,
+# сумма значений в котором минимальна. Напечатайте эту сумму.
+# Число k всегда больше нуля и меньше n.
+
+def find_min_slice_sum(data, elements_in_slice):
+    window_sum = sum(data[0:elements_in_slice])
+    min_sum = window_sum
+
+    for index in range(elements_in_slice, len(data)):
+        window_sum += data[index] - data[index - elements_in_slice]
+        min_sum = min(min_sum, window_sum)
+
+    return min_sum
+
+
+if __name__ == '__main__':
+    data = [5, -3, -2, 10, 2, 7, 1, -6, 13]
+    elements_in_slice = 4
+    print(find_min_slice_sum(data, elements_in_slice))
