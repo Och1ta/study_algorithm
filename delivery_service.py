b3547a550038702b0f4551227f96fabacbea4ebf
Anton Kabanov <imcreativchik@gmail.com>
2023-10-15 23:45:31 +0500

    refactor review`s edits

--- delivery_service.py
+++ delivery_service.py
@@ -1,28 +1,34 @@
-# input
-# В первой строке записан массив целых чисел, через пробел — это вес отдельных роботов.
-# Во второй строке записан лимит — предельная грузоподъёмность платформы.
-# output
-# Целое число, указывающее на необходимое количество платформ для транспортировки.
+def delivery_service(weights: list[int], limit: int) -> int:
+    # input
+    # В первой строке записан массив целых чисел, через пробел — это вес отдельных роботов.
+    # Во второй строке записан лимит — предельная грузоподъёмность платформы.
+    # output
+    # Целое число, указывающее на необходимое количество платформ для транспортировки.
 
-
-def delivery_service():
-    weights = list(map(int, input().split()))
-    limit = int(input())
-    weights.sort()  # Сортируем веса роботов по возрастанию
+    weights: list[int] = sorted(weights)  # Сортируем веса роботов по возрастанию
     platforms_count: int = 0
+    left_pointer: int = 0
+    right_pointer: int = len(weights) - 1
 
-    while weights:
-        heaviest_robot = weights.pop()  # Выбираем самого тяжелого робота
-
-        if weights and heaviest_robot + weights[0] <= limit:
-            # Если есть другие роботы и суммарный вес текущего и следующего робота не превышает лимит,
+    while left_pointer <= right_pointer:
+        # Пока левый указатель меньше правого
+        if weights[right_pointer] + weights[left_pointer] <= limit:
+            # Если суммарный вес крайнего левого и правого робота не превышает лимит,
             # то помещаем обоих на платформу
-            weights.pop(0)
+            platforms_count += 1  # Увеличиваем счетчик платформ
+            left_pointer += 1
+            right_pointer -= 1
 
-        platforms_count += 1  # Увеличиваем счетчик платформ
+        elif weights[right_pointer] or weights[left_pointer] == limit:
+            # Выбираем самого тяжелого робота
+            # на крайнем левом или правом положении
+            platforms_count += 1
+            right_pointer -= 1
 
-    print(platforms_count)
+    return platforms_count
 
 
 if __name__ == '__main__':
-    delivery_service()
+    weights = [int(x) for x in input().split()]
+    limit = int(input())
+    print(delivery_service(weights, limit))
